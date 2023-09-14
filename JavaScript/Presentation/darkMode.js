0d0049774ac656f7f257ad49bb257223342c09f7
Elyas <Elyas575@hotmail.com>
2023-09-14 07:32:50 +0300

    bug fix

--- JavaScript/Presentation/darkMode.js
+++ JavaScript/Presentation/darkMode.js
@@ -1,22 +1,25 @@
 const body = document.body;
 const outterContainer = document.getElementById("outerContainer");
 const textTheme = document.querySelector(".theme-text");
+const subTopics = document.getElementById("boxDetailsUpper");
 
-export function addDarkModeClasses () {
+export function addDarkModeClasses() {
   textTheme.innerText = "Light Mode";
   body.classList.add("dark-mode");
   outterContainer.classList.add("dark-mode");
-};
+  subTopics.classList.add("dark-mode");
+}
 
-export function removeDarkModeClasses(){
+export function removeDarkModeClasses() {
   body.classList.remove("dark-mode");
   outterContainer.classList.remove("dark-mode");
-};
- 
-export function DarkThemeHandler (themeColor){
+  subTopics.classList.remove("dark-mode");
+}
+
+export function DarkThemeHandler(themeColor) {
   if (themeColor === "DARK") {
     addDarkModeClasses();
   } else {
     removeDarkModeClasses();
   }
-};
+}
